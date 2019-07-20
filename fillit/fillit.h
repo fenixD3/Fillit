542f41df389666b64afb614540f6cf3996e3030c
Yeste Lila <ylila@il-e5.21-school.ru>
2019-07-20 18:54:12 +0300

    Start adding struct

--- fillit/fillit.h
+++ fillit/fillit.h
@@ -7,7 +7,17 @@
 # define BUFF_SIZE 550
 # include "libft/libft.h" // wrong (should be only libft.h!!!)
 
-void	ft_mkerr();
-int		ft_readfile(char *filename, char **line);
+typedef struct		s_dance
+{
+	char	*name;
+	int		coord;
+	s_dance	*up;
+	s_dance	*down;
+	s_dance	*left;
+	s_dance	*right;
+}					t_dance;
+
+void				ft_mkerr();
+int					ft_readfile(char *filename, char **line);
 
 #endif
