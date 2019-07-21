9da4f2c852af402052b72cc11067063ff113f1c8
Yeste Lila <ylila@oa-i1.21-school.ru>
2019-07-21 17:23:01 +0300

    New life
    ;

--- fillit/fillit.h
+++ fillit/fillit.h
@@ -8,16 +8,6 @@
 # define LEAD_DIGT 100
 # include "libft/libft.h" // wrong (should be only libft.h!!!)
 
-typedef struct		s_dance
-{
-	char			*name;
-	int				coord;
-	struct	s_dance	*up;
-	struct	s_dance	*down;
-	struct	s_dance	*left;
-	struct	s_dance	*right;
-}					t_dance;
-
 void				ft_mkerr();
 int					ft_readfile(char *filename, char **line);
 int					**mkfig_arr(const char *line, int numfig);
